49c82fb67a0d44d6955a31cb1093fe0b3ea79923
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-10-22 10:14:05 +0300

    реализовал интерфейс компонет контекст. Тем самым обработал переворот экрана

--- app/src/main/java/com/example/mvidecomposetest/presentation/DefaultContactListComponent.kt
+++ app/src/main/java/com/example/mvidecomposetest/presentation/DefaultContactListComponent.kt
@@ -1,25 +1,25 @@
 package com.example.mvidecomposetest.presentation
 
-import android.media.tv.AdRequest
+import com.arkivanov.decompose.ComponentContext
+import com.example.mvidecomposetest.core.componentScope
 import com.example.mvidecomposetest.data.RepositoryImpl
 import com.example.mvidecomposetest.domain.Contact
 import com.example.mvidecomposetest.domain.GetContactsUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class DefaultContactListComponent(
+    componentContext: ComponentContext,
     val onEditingContactRequested: (Contact) -> Unit,
     val onAddContactRequested: () -> Unit
-) : ContactListComponent {
+) : ContactListComponent, ComponentContext by componentContext{
 
     private val repository = RepositoryImpl
     private val getContactsUseCase = GetContactsUseCase(repository)
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
+    private val coroutineScope = componentScope()
     override val model: StateFlow<ContactListComponent.Model> = getContactsUseCase()
         .map { ContactListComponent.Model(it) }
         .stateIn(
