c97e7b9799479a99fb56ac2ead339d23dca4a6fc
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-10-22 10:13:03 +0300

    накинул анотацию парселайз

--- app/src/main/java/com/example/mvidecomposetest/presentation/Model.kt
+++ app/src/main/java/com/example/mvidecomposetest/presentation/Model.kt
@@ -1,6 +1,10 @@
 package com.example.mvidecomposetest.presentation
 
+import com.arkivanov.essenty.parcelable.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Model(
     val username: String,
     val phone: String
-)
+): Parcelable
