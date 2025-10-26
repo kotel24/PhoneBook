5decdbc0706d45baa6bf82a841696607d746371b
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-10-26 22:03:00 +0300

    дополнил фабрику для добавления контакта

--- app/src/main/java/com/example/mvidecomposetest/presentation/AddContactStoreFactory.kt
+++ app/src/main/java/com/example/mvidecomposetest/presentation/AddContactStoreFactory.kt
@@ -3,16 +3,19 @@ package com.example.mvidecomposetest.presentation
 import com.arkivanov.mvikotlin.core.store.Reducer
 import com.arkivanov.mvikotlin.core.store.Store
 import com.arkivanov.mvikotlin.core.store.StoreFactory
-import com.example.mvidecomposetest.presentation.AddContactStore.Intent
+import com.arkivanov.mvikotlin.extensions.coroutines.CoroutineExecutor
+import com.example.mvidecomposetest.domain.AddContactUseCase
 
 class AddContactStoreFactory (
-    private val storeFactory: StoreFactory
+    private val storeFactory: StoreFactory,
+    private val addContactUseCase: AddContactUseCase
 ) {
     private val store: Store<AddContactStore.Intent, AddContactStore.State, AddContactStore.Label> =
         storeFactory.create(
             name = "AddContactStoreFactory",
             initialState = AddContactStore.State(username = "", phone = ""),
-
+            executorFactory = {ExecutorImpl()},
+            reducer = ReducerImpl
         )
 
     private sealed interface Action
@@ -22,6 +25,27 @@ class AddContactStoreFactory (
         data class ChangeUsername(val username: String): Message
 
         data class ChangePhone(val phone: String): Message
+    }
+
+    private inner class ExecutorImpl: CoroutineExecutor<AddContactStore.Intent, Action, AddContactStore.State, Message, AddContactStore.Label>(){
+        override fun executeIntent(
+            intent: AddContactStore.Intent,
+            getState: () -> AddContactStore.State
+        ) {
+            when(intent){
+                is AddContactStore.Intent.ChangePhone -> {
+                    dispatch(Message.ChangePhone(phone = intent.phone))
+                }
+                is AddContactStore.Intent.ChangeUsername -> {
+                    dispatch(Message.ChangePhone(phone = intent.username))
+                }
+                AddContactStore.Intent.SaveContact -> {
+                    val state = getState()
+                    addContactUseCase(username = state.username, phone = state.phone)
+                    publish(AddContactStore.Label.ContactSaved)
+                }
+            }
+        }
     }
 
     private object ReducerImpl: Reducer<AddContactStore.State, Message>{
